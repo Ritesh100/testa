bd5c6b11fe6bb36b4dcc2ca765b90c3963359b30
ritesh <reteshthapakaji@gmail.com>
2020-12-22 12:26:43 +0545

    change2

--- data.php
+++ data.php
@@ -12,6 +12,6 @@ if (isset($_POST['submit'])) {
   $message2 = "You have received a  appointment  from ". $name .".\n\n".$Number.".\n\n".$symptioms."\n\n".$date."\n\n".$date."\n\n".$time;
     mail($myMail, $subject, $message2, $header);
     echo '<script>alert("Appointment requesst successfully ! ")</script>';
-    echo '<script>window.location.href="appointment.php";</script>';
+    echo '<script>window.location.href="index.html";</script>';
 }
  ?>
